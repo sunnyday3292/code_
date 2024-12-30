5d8c2651635459aa924a82b9dcdee87c0f5b02a1
seohee <164632290+sunnyday3292@users.noreply.github.com>
2024-12-30 19:49:13 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int a,b,c;
+	while (true) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) {
+			break;
+		}
+		if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) {
+			cout << "right"<<"\n";
+		}
+		else {
+			cout << "wrong"<<"\n";
+		}
+	}
+	
+
+}
