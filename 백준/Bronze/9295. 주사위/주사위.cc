8300775b26b5f355592b72c756d81d93153032a6
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-01-04 06:06:30 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,a,b;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		cout << "Case " << i + 1 << ": " << a + b<<"\n";
+	}
+
+	
+}
+	
