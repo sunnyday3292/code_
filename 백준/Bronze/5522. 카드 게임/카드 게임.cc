3ba35f093fc4e03990664d943d1e99741d1f635f
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-01-03 17:47:50 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k;
+    int s = 0;
+    for (int i = 0; i < 5; i++) {
+        cin >> k;
+        s += k;
+    }
+    cout << s;
+    
+}
+
