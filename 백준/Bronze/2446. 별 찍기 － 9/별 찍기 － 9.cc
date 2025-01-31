7241ff9cec95d7538e2da897f79554be202c5cf9
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-01 03:01:53 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;;
+int arr[9][9];
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+
+		for (int j = 1; j<i; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= 2*(n-i)+1; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+
+	}
+	for (int i = n-1; i >= 1; i--) {
+
+		for (int j = 1; j < i; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= 2 * (n - i) + 1; k++) {
+			cout << "*";
+		}
+		cout << "\n";
+
+	}
+
+	
+}
