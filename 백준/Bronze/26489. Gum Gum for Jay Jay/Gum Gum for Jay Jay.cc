ce83617ff3c1fd033767a9e424da4849b7ead826
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-04 18:13:08 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;;
+const double PI = 3.1415927;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string s;
+	int cnt = 0;
+	while (getline(cin, s)) {
+		cnt++;
+
+	}
+	cout << cnt;
+	
+	
+}
