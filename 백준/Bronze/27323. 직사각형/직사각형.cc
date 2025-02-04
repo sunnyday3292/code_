815817a835955b166ddfe4985036cb72077e555c
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-04 18:44:09 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;;
+const double PI = 3.1415927;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int t, s;
+	cin >> t >> s;
+	cout << t * s;
+}
