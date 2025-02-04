12195fb67220228e66b8286c13ed47604808a44a
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-04 23:13:29 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;;
+const double PI = 3.1415927;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int x;
+	cin >> x;
+	if (x % 7 == 2) cout << 1;
+	else cout << 0;
+	
+}
