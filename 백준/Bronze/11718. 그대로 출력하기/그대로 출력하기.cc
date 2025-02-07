322ecd0980a7ecb48099aa3ee758f439675b7f94
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-07 13:58:15 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string s;
+	while (!cin.eof()) {
+		getline(cin,s);
+		cout<<s<<"\n";
+	}
+	
+}
