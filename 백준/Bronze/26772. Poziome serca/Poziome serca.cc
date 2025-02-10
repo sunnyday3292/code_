88c714146e79b5c625025180b5f1e71f63bf6543
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-11 06:39:46 +0900

    [Bronze IV] Title: Poziome serca, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << " @@@   @@@  ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "@   @ @   @ ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "@    @    @ ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "@         @ ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << " @       @  ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "  @     @   ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "   @   @    ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "    @ @     ";
+	}
+	cout << "\n";
+	for (int i = 0; i < n; i++) {
+		cout << "     @      ";
+	}
+
+
+
+	
+}
