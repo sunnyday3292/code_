c7c1eeae84791397341663f93f36b30754ecee02
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-10 11:08:26 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int arr[3];
+	cin >> arr[0] >> arr[1] >> arr[2];
+	sort(arr, arr + 3);
+	cout << arr[1] + arr[2];
+}
