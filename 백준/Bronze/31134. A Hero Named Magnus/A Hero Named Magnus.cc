594b6e2fe58c69849e4e732258edf87b1d199f4e
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-11 22:50:41 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	long long int t,n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << 2 * n - 1<<"\n";
+
+	}
+	
+
+}
