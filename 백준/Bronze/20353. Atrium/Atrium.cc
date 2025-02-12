5f3882b86491f97b4a4fbc34b384094ce163473f
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-13 03:06:19 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    double n; cin >> n;
+    cout << fixed;
+    cout.precision(7);
+    cout << sqrt(n) * 4;
+
+}
