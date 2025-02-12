7e669cb0cb98640825ca65b007bfee4cb6a46e7e
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-02-12 18:47:56 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<set>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+
+		for (int j = 1; j <= n; j++) {
+			cout << "a";
+		}
+	
+	
+}
