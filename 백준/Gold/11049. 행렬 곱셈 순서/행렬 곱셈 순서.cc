48aac7938ecad9c48f4e87588c6063154bea3c4b
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-03-10 01:22:40 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 28 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include <climits>
+using namespace std;;
+int N;
+int m[501][2];
+int dp[501][501];
+
+int main() {
+    cin>>N;
+    for(int i=1;i<=N;i++) cin>>m[i][0]>>m[i][1];
+
+    for(int i=1; i<N; i++){
+        for(int j=1; i+j<=N; j++){
+            dp[j][i+j] = INT_MAX;
+            for(int k=j; k<=i+j; k++){
+                dp[j][i+j] = min(dp[j][i+j], dp[j][k] + dp[k+1][i+j] + m[j][0]*m[k][1]*m[i+j][1]);
+            }
+        }
+    }
+ 
+    cout << dp[1][N];
+    
+}
