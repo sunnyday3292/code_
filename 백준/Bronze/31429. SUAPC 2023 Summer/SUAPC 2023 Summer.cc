170c66cc026d0f28dd2311cd3ecdc1cf25e6e171
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-04-06 04:18:38 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    if(n==1)cout<<12<<" "<<"1600";
+    if(n==2)cout<<11<<" "<<"894";
+    if(n==3)cout<<11<<" "<<"1327";
+    if(n==4)cout<<10<<" "<<"1311";
+    if(n==5)cout<<9<<" "<<"1004";
+    if(n==6)cout<<9<<" "<<"1178";
+    if(n==7)cout<<9<<" "<<"1357";
+    if(n==8)cout<<8<<" "<<"837";
+    if(n==9)cout<<7<<" "<<"1055";
+    if(n==10)cout<<6<<" "<<"556";
+    if(n==11)cout<<6<<" "<<"773";
+}
