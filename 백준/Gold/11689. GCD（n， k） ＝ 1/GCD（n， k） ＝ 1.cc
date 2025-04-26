b8abff9373b22c3bcea51ca88f722950a7c2fe38
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-04-27 01:48:08 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;;
+
+long long int phi(long long int n){
+    long long int ans=n;
+    for(long long int i=2;i*i<=n;i++){
+        if(n%i==0){
+            while(n%i==0){
+                n/=i;
+            }
+            ans=ans-ans/i;
+        }
+    }
+    if(n>1) ans-=ans/n;
+    return ans;
+}
+int main(){
+    long long int n;
+    cin>>n;
+    cout<<phi(n);
+    return 0;
+}
