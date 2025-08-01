630c2c2e3d3a52e01b69cd74a6bd608a8445efae
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-08-01 19:09:56 +0900

    [Gold V] Title: 선 긋기, Time: 356 ms, Memory: 26728 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n;
+long long x, y;
+vector<pair<long long, long long> > v;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++){
+        long long a, b;
+        cin >> a >> b;
+        v.push_back(make_pair(a, b));
+    }
+    sort(v.begin(), v.end()); //pair를 sort시 앞 원소를 기준으로 정렬됨
+
+    long long sum = 0;
+    long long l = v[0].first;
+    long long r = v[0].second;
+
+    for(int i=1; i<n; i++){
+        if(r < v[i].first) { // 시작점이 직전 끝점보다 큼 (겹치지 않음)
+            sum += r-l;
+            l = v[i].first;
+            r = v[i].second;
+        } else { // 현재 시작점이 직전 끝점보다 작거나 같음
+            if(r < v[i].second) // 현재 끝점이 직전 끝점보다 큼 (선분 연장됨)
+                r = v[i].second;
+        }
+    }
+    sum += r-l; // 마지막 Case
+    cout << sum;
+    return 0;
+}
