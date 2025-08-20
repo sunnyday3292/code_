46567339cb1fa7681631a799f1a9001898a54401
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-08-20 18:38:44 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
 #define MOD 1000000009
-int arr[1000001];
+int dp[1000001];
 
 int main(){
     ios::sync_with_stdio(false);
-	cin.tie(0);
+		cin.tie(0);
     
     int T; cin>>T;
-    arr[1]=1; arr[2]=2;arr[3]=4;
+    dp[1]=1; dp[2]=2;dp[3]=4;
     
     for(int i=4;i<=1000000;i++){
-        arr[i]=((arr[i-1]+arr[i-2])%MOD+arr[i-3])%MOD;
+        dp[i]=((dp[i-1]+dp[i-2])%MOD+dp[i-3])%MOD;
     } 
     while(T--){
         int n; cin>>n;
-        cout<<arr[n]<<"\n";     
+        cout<<dp[n]<<"\n";     
     }
     return 0;
 }
