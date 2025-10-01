ca22265097f81a465169d73bbb3eef0e3bdb2ba2
seohee <164632290+sunnyday3292@users.noreply.github.com>
2025-10-01 23:51:58 +0900

    [Silver I] Title: 2-SAT - 2, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11278. 2－SAT － 2/2－SAT － 2.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int,int>> clauses;
+
+bool check(vector<int>& assignment){
+    for(auto [a,b]:clauses){
+        bool x=(a>0?assignment[a]:!assignment[-a]);
+        bool y=(b>0?assignment[b]:!assignment[-b]);
+        if(!(x||y)) return false;
+    }
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N,M;
+    cin >> N >> M;
+    clauses.resize(M);
+    for(int i=0;i<M;i++){
+        cin >> clauses[i].first >> clauses[i].second;
+    }
+
+    vector<int> assignment(N+1,0);
+    for(int mask=0; mask<(1<<N); mask++){
+        for(int i=1;i<=N;i++){
+            assignment[i] = (mask>>(i-1)) & 1;
+        }
+        if(check(assignment)){
+            cout << 1 << "\n";
+            for(int i=1;i<=N;i++){
+                cout << assignment[i] << " ";
+            }
+            cout << "\n";
+            return 0;
+        }
+    }
+    cout << 0 << "\n";
+    return 0;
+}
