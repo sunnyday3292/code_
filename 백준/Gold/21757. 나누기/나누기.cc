b235357cb9f1dfe723a194945b4f388847f561c0
seohee <164632290+sunnyday3292@users.noreply.github.com>
2026-01-27 17:17:17 +0900

    [Gold II] Title: 나누기, Time: 4 ms, Memory: 2680 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Gold/21757. 나누기/나누기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N;
+    cin>>N;
+    int A[100001],S[100001];
+    long long dp[5];
+    S[0]=0;
+    for(int i=1;i<=N;i++){
+        cin>>A[i];
+        S[i]=S[i-1]+A[i];
+    }
+    long long ans=S[N]/4;
+    if(S[N]%4!=0){
+        cout<<0<<'\n';
+        return 0;
+    }
+
+    dp[0]=1;
+    int ret=0;
+    for(int i=1;i<=N;i++){
+        int t=S[i]/ans;
+        if(S[i]%ans!=0||t==0||t>4)continue;
+        else{
+            dp[t]+=dp[t-1];
+        }
+        ret=dp[4];
+    }
+    cout<<ret;
+    
+    return 0;
+}
